862b048bf25f67a8473d9ae23fdd720d38132aa6
111sniper111 <pranavchoudhary567@gmail.com>
2026-01-14 12:07:06 +0530

    playermovement

---
+++ Assets/scripts/mousemovement.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Mousemovement : MonoBehaviour
+{
+    public float mouseSensitivity = 500f;
+
+    float xRotation = 0f;
+    float yRotation = 0f;
+
+    public float topClamp = -90f;
+    public float bottomClamp = 90f;
+
+    void Start()
+    {
+        //LOCKING THE CURSOR
+        Cursor.lockState = CursorLockMode.Locked;
+
+    }
+
+    void Update()
+    {
+        //getting the mouse input
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        //rotation arounf the x  axis (loook up adn down)
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
+
+        yRotation += mouseX;
+
+        transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
+    }
+}
